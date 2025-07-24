28c15765a5f8c18df8021365dc72165b25849ede
Dante <n8n@bk.ru>
2025-07-24 07:10:47 +0000

    added cors

--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@ import express from 'express';
 import { chatCompletion } from './chat.js';
 
 const app = express();
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); 
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+  // Обработка предварительного OPTIONS-запроса
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  
+  next();
+});
 app.use(express.json());
 
 app.post('/v1/chat/completions', chatCompletion);
